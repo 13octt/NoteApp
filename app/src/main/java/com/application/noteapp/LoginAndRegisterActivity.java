3a32b642cead4754f5c00cdc7f80972485751d1f
bao duy <20521231@gm.uit.edu.vn>
2023-01-10 19:54:41 +0700

    login and register screen

---
+++ app/src/main/java/com/application/noteapp/LoginAndRegisterActivity.java
@@ -0,0 +1,34 @@
+package com.application.noteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LoginAndRegisterActivity extends AppCompatActivity {
+
+    Button btnLogin, btnRegiter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_and_register);
+
+        // Reference
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnRegiter = (Button) findViewById(R.id.btn_register);
+
+        // Go to login screen
+        btnLogin.setOnClickListener(view ->{
+            Intent loginScreen = new Intent(LoginAndRegisterActivity.this, LoginActivity.class);
+            startActivity(loginScreen);
+        });
+
+        // Go to register screen
+        btnRegiter.setOnClickListener(view -> {
+            Intent registerScreen = new Intent(LoginAndRegisterActivity.this, RegisterActivity.class);
+            startActivity(registerScreen);
+        });
+    }
+}
